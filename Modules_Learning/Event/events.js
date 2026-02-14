e049099ba261a274053e8f2a37bc75243be95caa
unknown <arnab0574@gmail.com>
2026-02-15 03:29:09 +0600

    Event raise and Listen

---
+++ Modules_Learning/Event/events.js
@@ -0,0 +1,38 @@
+// const EventEmitter = require('events')
+
+const emitter = new EventEmitter()
+
+// Register a Listener for 'Fire' event
+// emitter.on('Fire', () =>{
+//     console.log("We need to run!")
+// })
+
+// Raise an Event
+// emitter.emit('Fire')  
+
+
+// emitter.on('Fire', () =>{
+//     console.log("We need to run!")
+// })
+
+
+// // The event will rise 2sec later, and then the action will be performed
+// setTimeout(() => {
+//     emitter.emit('Fire')
+// }, 2000);
+
+
+
+//Multiple perametre
+emitter.on('Fire', ({period, text}) =>{
+    console.log(`We need to run because ${period} ${text}`)
+})
+
+
+// The event will rise 2sec later, and then the action will be performed
+setTimeout(() => {
+    emitter.emit('Fire', {
+        period : 'first',
+        text : 'period ended',
+    });
+}, 2000);
