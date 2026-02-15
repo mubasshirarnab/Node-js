2b996b943c965ce08cacf85d520582e067580d72
unknown <arnab0574@gmail.com>
2026-02-15 18:27:59 +0600

    ReadStream from a server.

--- Stream&Buffer/index.js
+++ Stream&Buffer/index.js
@@ -1,9 +1,38 @@
+const http = require('http');
+const { chunk } = require('lodash');
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        res.write('<html><head><title>Form</title></head>')
+        res.write('<body><form method="post" action = "/process"><input name="text" /></form></body>')
+        res.end()
+    }
+    else if(req.url === '/process' && req.method === 'POST'){
+        req.on('data', (chunk) =>{
+            console.log(chunk.toString())
+        })
+
+        res.write("Thank you for submitting your data")
+        res.end()
+    }
+    else{
+        res.write("Page Not Found.")
+        res.end()
+    }
+});
+
+server.listen(3000)
+
+console.log("The server is running on port 3000.....")
+
+
+
 //Reading from file using "ReadStream"
-const fs = require('fs')
-const { chunk } = require('lodash')
+// const fs = require('fs')
+// const { chunk } = require('lodash')
 
-const ourReadStream = fs.ReadStream(`${__dirname}/bigdata.txt`)
+// const ourReadStream = fs.ReadStream(`${__dirname}/bigdata.txt`)
 
-ourReadStream.on('data', (chunk) =>{
-    console.log(chunk.toString())
-})
+// ourReadStream.on('data', (chunk) =>{
+//     console.log(chunk.toString())
+// })
